7264e13b4d919861c36d693cf4e78aaa544633ad
Cornelis Plomp <cplomp91@gmail.com>
2020-09-14 21:01:39 -0400

    Main page works, button click works (doesn't do much)

--- server.js
+++ server.js
@@ -4,17 +4,23 @@ const fs = require('fs');
 process.env.PORT = 8080;
 const port = process.env.PORT;
 
+
 http.createServer(function (req, res) {
-    res.writeHead(200, { 'Content-Type': 'text/html' });
-    fs.readFile("mainPage.html", (err, filedata) => {
-        if (err) {
-            console.error(err);
-            res.writeHead(404, { 'Content-Type': 'text/plain' });
-            res.write('404 - HTML file not found');
-        } else {
-            res.writeHead(200, { 'Content-Type': 'text/html' });
-            res.write(filedata.toString());
-        }
+    const url = req.url
+    if (url === "/") {
+        res.writeHead(200, { 'Content-Type': 'text/html' });
+        fs.readFile("mainPage.html", (err, filedata) => {
+            if (err) {
+                console.error(err);
+                res.writeHead(404, { 'Content-Type': 'text/plain' });
+                res.write('404 - HTML file not found');
+            } else {
+                res.writeHead(200, { 'Content-Type': 'text/html' });
+                res.write(filedata.toString());
+            }
+            res.end();
+        });
+    } else {
         res.end();
-    });
+    }  
 }).listen(port);
